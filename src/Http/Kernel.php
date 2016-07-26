4c6be39e1c2163e207a34f32f22257c589508c88
TheCrott <aldothecrott@gmail.com>
2016-07-26 22:42:01 +0700

    add route collection
    add route collection from symfony

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -3,13 +3,22 @@
 namespace BelajarOOP\EpiphanyFramework\Http;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 class Kernel implements HttpKernelInterface
 {
   public function __construct()
   {
-    // todo
+    // Symfony Route Collection
+    $this->router = new RouteCollection();
   }
 
   public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true)
